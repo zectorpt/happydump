0f7f8afbce20a8d30a3810537268ebd6902364a6
José Almeida <josemedeirosdealmeida@gmail.com>
2021-12-11 20:52:14 +0000

    Update happydump.sh

--- happydump.sh
+++ happydump.sh
@@ -2,3 +2,8 @@
 echo "First step to the moon! And welcome! ;)"
 kubectl create namespace happydump
 kubectl get pods -o wide --namespace=happydump
+
+
+kubectl get nodes| awk '{print $1}'| grep -wv NAME > ALLNODES.var
+nodearray < ALLNODES.var
+printf '%s' "${nodearray[0]}"
