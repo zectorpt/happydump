9581e8a3214af07efc75c29141ea17dd75df4ea5
Jose Almeida <josemedeirosdealmeida@gmail.com>
2022-01-05 15:08:11 +0000

    Working to access node

--- happydump.sh
+++ happydump.sh
@@ -3,7 +3,7 @@ wget -q http://www.brainyquote.com/link/quotebr.js -O -|grep -o "\"[A-Z].*\."
 kubectl create namespace happydump
 sleep 1
 kubectl create deployment --image=nginx nginxtodelete --namespace=happydump
-sleep 3
+sleep 1
 kubectl get pods -o wide --namespace=happydump
 
 
@@ -16,12 +16,43 @@ for ((i = 0; i < ${#MAPFILE[@]}; ++i)); do
     echo "$position - ${MAPFILE[$i]}"
 done
 echo "Please chose the NODE number to TCP DUMP: "
-sleep 1
+
+#sleep 1
+
 read node
+newnode=${MAPFILE[$node]}
+newnode=${newnode::-1}
 
+DEFINITIONS="$(cat <<EOF
+{
+  "spec": {
+    "nodeName": "$newnode",
+    "hostPID": true,
+    "containers": [
+      {
+        "securityContext": {
+          "privileged": true
+        },
+        "image": "alpine",
+        "name": "nsenter",
+        "stdin": true,
+        "stdinOnce": true,
+        "tty": true,
+        "command": [ "nsenter", "--target", "1", "--mount", "--uts", "--ipc", "--net", "--pid", "--", "bash", "-l" ]
+      }
+    ]
+  }
+}
+EOF
+)"
+
+echo $DEFINITIONS
+
+sleep 1
 #kubectl debug node/${MAPFILE[0]} -it --image=ubuntu --namespace=happydump
-kubectl debug node/${MAPFILE[$node]} -it --image=ubuntu --namespace=happydump
+#kubectl debug node/${MAPFILE[$node]} -it --image=ubuntu --namespace=happydump
 #kubectl debug node/aks-nodepool4-92987211-vmss000031 -it --image=ubuntu --namespace=happydump
+kubectl run --rm --image alpine --overrides="$DEFINITIONS" -ti ${MAPFILE[$node]} --namespace=happydump
 
-sleep 3
+rm -f ALLNODES.var
 kubectl delete namespaces happydump
